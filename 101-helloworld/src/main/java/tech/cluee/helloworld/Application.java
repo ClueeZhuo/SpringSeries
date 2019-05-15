072fd8d8fe0c0442c4c3d5798cc0ac2d25946b88
ClueeZhuo <clueezhuo@outlook.com>
2019-05-15 17:53:35 +0800

    Add eg.

--- 101-helloworld/src/main/java/tech/cluee/helloworld/Application.java
+++ 101-helloworld/src/main/java/tech/cluee/helloworld/Application.java
@@ -1,7 +1,15 @@
 package tech.cluee.helloworld;
 
+import java.lang.Thread.State;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Stack;
+
+import javax.activation.MailcapCommandMap;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableLoadTimeWeaving;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,10 +20,37 @@ public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
-	
+
 	@RequestMapping("/hellojava")
 	public String helloJava() {
 		return "Hello, Java!";
 	}
 
+	private static final Map<Character, Character> brackets = new HashMap<Character, Character>();
+	static {
+		brackets.put(')', '(');
+		brackets.put('}', '{');
+		brackets.put(']', '[');
+	}
+
+	public static boolean isMatch(String str) {
+
+		if (str == null) {
+			return false;
+		}
+
+		Stack<Character> stack = new Stack<Character>();
+
+		for (Character ch : str.toCharArray()) {
+			if (brackets.containsValue(ch)) {
+				stack.push(ch);
+			} else if (brackets.containsKey(ch)) {
+				if (stack.isEmpty() || stack.pop() != brackets.get(ch)) {
+					return false;
+				}
+			}
+		}
+		
+		return stack.isEmpty();
+	}
 }
